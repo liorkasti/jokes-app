adb0bd55b0933b63aacd706f489bda9d41d9013b
Lior Kastenbaum <liorkasti@gmail.com>
2021-07-15 17:51:32 +0300

    Add bottom tabs navigarion-v0.1

---
+++ src/navigation/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import NavigationTabs from './tabs'
+import { theme } from '../core/theme'
+import { StartScreen, LoginScreen, RegisterScreen, Dashboard, JokeList } from '../screens'
+
+const Stack = createStackNavigator()
+
+export default function AppNavigator() {
+  return (
+    <Provider theme={theme}>
+      {/* <AppContainer /> */}
+      <NavigationContainer>
+        <Stack.Navigator
+          // initialRouteName="StartScreen"
+          // initialRouteName="Dashboard"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Dashboard" component={NavigationTabs} />
+          {/* <Stack.Screen name="StartScreen" component={StartScreen} /> */}
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          {/* <Stack.Screen name="Dashboard" component={Dashboard} /> */}
+          <Stack.Screen name="JokeList" component={JokeList} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
